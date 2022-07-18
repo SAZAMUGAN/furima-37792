92497410b438b14597abb173433283c4d0614bbc
SAZAMUGAN <hamagami1171@gmail.com>
2022-07-18 22:02:43 +0900

    KenNameモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,10 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :burden_of_shipping_charges
+  belongs_to :ken_name
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :burden_of_shipping_charges_id, numericality: { other_than: 1, message: "can't be blank" }
-
+  validates :ken_name_id, numericality: { other_than: 1, message: "can't be blank" }
 end
