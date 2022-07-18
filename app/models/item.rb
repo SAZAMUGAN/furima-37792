545a7748dc59c1436d817c34f133f34668b8a682
SAZAMUGAN <hamagami1171@gmail.com>
2022-07-18 22:08:37 +0900

    DaysToShipモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,12 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :burden_of_shipping_charges
   belongs_to :ken_name
+  belongs_to :days_to_ship
+  
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :burden_of_shipping_charges_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :ken_name_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }
 end
