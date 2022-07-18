4cf21a5a1f3a5afe9f65535707cfada787352e70
SAZAMUGAN <hamagami1171@gmail.com>
2022-07-18 21:54:56 +0900

    BurdenOfShppingChargesモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :burden_of_shipping_charges
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :burden_of_shipping_charges_id, numericality: { other_than: 1, message: "can't be blank" }
 
 end
