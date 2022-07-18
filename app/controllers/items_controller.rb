4ea536c0f60cb4a113272b32d130d8374dd5f712
SAZAMUGAN <hamagami1171@gmail.com>
2022-07-18 21:38:45 +0900

    商品保存機能の実装(少し)
    itemsコントローラー内のcreateアクションとストロングパラメーターの設定、そしてそれに伴うnew.html.erbの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,12 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Prototype.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,6 +26,11 @@ class ItemsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :burden_of_shipping_charges_id, :ken_name_id, :days_to_ship_id,
+       :price).merge(user_id: current_user.id)
 
 
 end
