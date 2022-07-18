1720e47daa908b9c87aae602eff03298887e4541
SAZAMUGAN <hamagami1171@gmail.com>
2022-07-18 18:37:35 +0900

    Categoryモデルに関するアソシエーション・バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+
 end
