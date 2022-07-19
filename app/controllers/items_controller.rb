368a4d0a9daddc9ce79cc4ec0d2ad35715d40456
SAZAMUGAN <hamagami1171@gmail.com>
2022-07-19 20:36:19 +0900

    商品保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+
   def index
   end
 
@@ -7,7 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Prototype.new(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
