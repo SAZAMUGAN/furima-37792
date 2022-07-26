175588eded1778cad214dcdf13a27d2c7dd982d8
SAZAMUGAN <hamagami1171@gmail.com>
2022-07-26 20:49:31 +0900

    エラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, except: [:index, :new, :create]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
