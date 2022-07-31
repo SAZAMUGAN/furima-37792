ba17abfde29d83a2226d7e41512c6f442bbd2ead
SAZAMUGAN <hamagami1171@gmail.com>
2022-07-31 18:52:43 +0900

    アソシエーション記述の修正と購入記録と配送先情報の保存機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,25 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @order_shipping_address = OrderShippingAddress.new
   end
+
+  def create
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if @order_shipping_address.valid?
+      @order_shipping_address.save 
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+
+  end
+
+  private
+  def order_params
+    params.require(:order_shipping_address).permit(:post_code, :ken_name_id, :city_name, :block, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,11 +1,4 @@
 class ShippingAddress < ApplicationRecord
-  belongs_to: order
-
-  validates :post_code, presence: true
-  validates :ken_name_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :city_name, presence: true
-  validates :block, presence: true
-  validates :building_name, presence: true
-  validates :phone_number, presence: true
+  belongs_to :order
 
 end
