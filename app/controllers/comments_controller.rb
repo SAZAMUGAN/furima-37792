d7949a4674e4f335d5bb2e2db64785a0d73696d6
SAZAMUGAN <hamagami1171@gmail.com>
2022-08-11 11:38:06 +0900

    コメント投稿機能の実装(同期通信)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def create
+  @comment = Comment.new(comment_params)
+    @item = Item.find(params[:item_id]) 
+      if @comment.save
+        redirect_to item_path(@item.id)
+        # CommentChannel.broadcast_to @item, { comment: @comment, user: @comment.user }
+      else
+        @item = @comment.item
+        @comments = @item.comments
+        render template: "items/show"
+      end
+  end
+  
+private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
