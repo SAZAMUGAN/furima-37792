27c628352084aa0401e09520b98cee2c92f71aeb
SAZAMUGAN <hamagami1171@gmail.com>
2022-08-14 21:37:33 +0900

    不要な記述の削除

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
-  // 新規投稿・編集ページのフォームを取得
   const postForm = document.getElementById('item-form');
   const previewList = document.getElementById('previews');
-  // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!postForm) return null;
   const fileField = document.querySelector('input[type="file"][name="item[image]"]');
   fileField.addEventListener('change', function(e){
@@ -12,10 +10,8 @@ document.addEventListener('DOMContentLoaded', function(){
     };
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
-    // 画像を表示するためのdiv要素を生成
     const previewWrapper = document.createElement('div');
     previewWrapper.setAttribute('class', 'preview');
-    // 表示する画像を生成
     const previewImage = document.createElement('img');
     previewImage.setAttribute('class', 'preview-image');
     previewImage.setAttribute('src', blob);
