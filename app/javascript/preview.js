9006fbed1e8c9ea6cb8abe1725a3922d0faa2bc1
SAZAMUGAN <hamagami1171@gmail.com>
2022-08-14 21:35:17 +0900

    画像のプレビュー機能の実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+  // 新規投稿・編集ページのフォームを取得
+  const postForm = document.getElementById('item-form');
+  const previewList = document.getElementById('previews');
+  // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
+  if (!postForm) return null;
+  const fileField = document.querySelector('input[type="file"][name="item[image]"]');
+  fileField.addEventListener('change', function(e){
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    // 画像を表示するためのdiv要素を生成
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    // 表示する画像を生成
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+  });
+});
